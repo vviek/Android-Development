99a1192c66f577e5201c9c910623ba0f531449ac
SOURABHPATEL12 <sp824768@gmail.com>
2026-02-16 18:25:17 +0530

    add dialog to add details in recyclerView

--- Recycler_view/app/src/main/java/com/sourabh/recyclerview/ContectModel.java
+++ Recycler_view/app/src/main/java/com/sourabh/recyclerview/ContectModel.java
@@ -3,8 +3,11 @@ package com.sourabh.recyclerview;
 public class ContectModel {
     public int img;
 
+
+
     public String  contect;
     public String entercontect;
+
 
          public ContectModel(int img,String contectt,String entercontectt){
 
@@ -13,5 +16,6 @@ public class ContectModel {
                        this.entercontect=entercontectt;
 
          }
+
 
 }
--- Recycler_view/app/src/main/java/com/sourabh/recyclerview/MainActivity.java
+++ Recycler_view/app/src/main/java/com/sourabh/recyclerview/MainActivity.java
@@ -1,6 +1,10 @@
 package com.sourabh.recyclerview;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -60,6 +66,59 @@ public class MainActivity extends AppCompatActivity {
         RecyclerContectAdopter adopter = new RecyclerContectAdopter(this, arrcontect);
 
         recyclerView.setAdapter(adopter);
+
+        //  floating action button
+
+        FloatingActionButton btnopenDialog;
+        btnopenDialog = findViewById(R.id.fabAdd);
+
+        btnopenDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                 //making couston dialog
+                Dialog dialog =new Dialog(MainActivity.this);//caling tha object of dialog calsss ,parameter showing that dilog apare in this asctivity
+                dialog.setContentView(R.layout.activity_add_updatets);//conecting tha layout that is made for dialog
+
+                //connecting all view that are use
+                EditText edtnamediaglo = dialog.findViewById(R.id.edtnamediaglo) ;
+                EditText  edtnumberdialog  = dialog.findViewById(R.id.edtnumberdialog)  ;
+                Button  submitaction = dialog.findViewById(R.id. submitDialog);
+
+
+                //now submit button on click some acton perform using setonclicklistner
+
+                submitaction.setOnClickListener(new View.OnClickListener() {
+                    String name;
+                    String number;
+                    @Override
+
+
+                    public void onClick(View view) {
+
+                           name = edtnamediaglo.getText().toString();
+
+
+                         number = edtnumberdialog.getText().toString();
+
+                        arrcontect.add(new ContectModel(R.drawable.img1,name,number));//taking value in array
+
+
+                        adopter.notifyItemInserted(arrcontect.size()-1);//adding value in adopter in last position
+                        recyclerView.scrollToPosition(arrcontect.size()-1);//scrolling to last insert element
+
+                        dialog.dismiss();//to remove dilog  from screenafter adding details
+
+                          // notice the data is store during only in run time after reopen the app therer is no data
+                        //because there is no data base connection now
+                    }
+                });
+
+                dialog.show();
+
+
+            }
+        });
 
 
 
