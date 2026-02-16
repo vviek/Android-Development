5d8b52140af6007f411ccf5b3e856f7f011a5f5d
SOURABHPATEL12 <sp824768@gmail.com>
2026-02-16 11:10:40 +0530

    file name changes done

--- Recycler_view/app/src/main/java/com/sourabh/recyclerview/ContectRow.java
+++ Recycler_view/app/src/main/java/com/sourabh/recyclerview/ContectRow.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class contect_row extends AppCompatActivity {
+public class ContectRow extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
