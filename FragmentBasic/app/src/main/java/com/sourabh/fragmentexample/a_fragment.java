6803b12a7ba6787b36dbb7f5b7e9ec2907a1ac08
SOURABHPATEL12 <sp824768@gmail.com>
2026-02-20 12:13:59 +0530

    learning about static fragment basic

---
+++ FragmentBasic/app/src/main/java/com/sourabh/fragmentexample/a_fragment.java
@@ -0,0 +1,31 @@
+package com.sourabh.fragmentexample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class a_fragment extends Fragment {
+
+
+    public a_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_a_fragment, container, false);
+        TextView txtfagr1=view.findViewById(R.id.txtfagr1);
+        return view;
+    }
+}
---
+++ FragmentBasic/app/src/main/java/com/sourabh/fragmentexample/b_fragment.java
@@ -0,0 +1,31 @@
+package com.sourabh.fragmentexample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class b_fragment extends Fragment {
+
+
+
+    public b_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =inflater.inflate(R.layout.fragment_b_fragment, container, false);
+        TextView txtfrag2=view.findViewById(R.id.txtfagr2);
+        return view;
+
+    }
+}
