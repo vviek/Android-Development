6dc6c983f088fea31bd7a154d376211d6a07f418
SOURABHPATEL12 <sp824768@gmail.com>
2026-02-24 17:34:50 +0530

    simple login page by xml

---
+++ SimpleLoginExample/app/src/main/java/com/example/simpleloginpage/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.simpleloginpage;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.simpleloginexample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
