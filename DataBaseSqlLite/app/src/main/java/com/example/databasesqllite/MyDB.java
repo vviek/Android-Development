40a732ba408fa1c1513c0d4b71b9ac245575ea5a
SOURABHPATEL12 <sp824768@gmail.com>
2026-02-28 11:41:16 +0530

    set up the SQLite DataBase

--- DataBaseSqlLite/app/src/main/java/com/example/databasesqllite/MyDB.java
+++ DataBaseSqlLite/app/src/main/java/com/example/databasesqllite/MyDB.java
@@ -1,4 +1,56 @@
 package com.example.databasesqllite;
 
-public class MyDB {
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+// 1 = extends the class with SQLiteOpenHelper which give two override method and constructor
+public class MyDB extends SQLiteOpenHelper {
+
+    // alwalys put static and final to database name
+
+
+    // 3
+    private static final String DATABASE_NAME = "contacteDB";
+    private static final int DATABASE_VERSION= 1;
+
+    //tablle name
+    private static final String Table_contect = "contect";
+    //coloum names
+    private static final String key_id="id";
+    private static final String key_name="name";
+    private static final String key_number="number";
+
+
+//2
+    public MyDB(@Nullable Context context) {
+        //put factory = null
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+        // Query to create table and parameter in oncreate method is database
+    }
+
+
+    // 5 now database is create automatically
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        //4
+        //  Query to create table and parameter in oncreate method is database write query inside db.execSql();
+        //datatype = integer or text
+        // put coloum name in varable and use in queary
+        //  SQL query = create table tablename (c1 datatype ,c2 datatpe,c3 datatype)
+        // space after create tabele
+
+        db.execSQL(" CREATE TABLE "+ Table_contect + "(" + key_id +  "integer," + key_name  + "text," + key_number + "text" + " );" );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
